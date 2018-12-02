525a8364e562e10c2bf6a3657130b1d87cfac616
Aruneko <aruneko99@gmail.com>
2018-12-02 12:48:26 +0900

    use swagger ui

--- settings/urls.py
+++ settings/urls.py
@@ -39,5 +39,5 @@ urlpatterns = [
         schema_view.without_ui(cache_timeout=0),
         name="schema-json",
     ),
-    url(r"^", schema_view.with_ui("redoc", cache_timeout=0)),
+    url(r"^", schema_view.with_ui("swagger", cache_timeout=0)),
 ]
